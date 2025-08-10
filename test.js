91d824f5257e92272cde1457c93e0c3b5e5c93f1
Lee Suah <dltndk332@gmail.com>
2025-08-11 02:53:54 +0900

    Add files via upload
    테스트 예제

---
+++ test.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+/*
+axios
+  .get("http://127.0.0.1:3000/inquiry_answer", {
+    params: { corpus: "알림 설정은 어떻게 설정하나요" },
+  })
+  .then((res) => {
+    console.log("응답:", res.data);
+  })
+  .catch((err) => {
+    if (err.response) {
+      console.error("상태:", err.response.status);
+      console.error("바디:", err.response.data);
+    } else {
+      console.error("에러:", err.message);
+    }
+  });
+*/
+/*
+axios
+  .get("http://127.0.0.1:3000/inquiry_answer", {
+    params: {
+      corpus: "이 약은 상호작용하는 약이 있나요?", // 엑셀 9번 질문 내용
+      //entpName: "한미약품", // 업체명
+      itemName: "타이레놀", // 제품명
+    },
+  })
+  .then((res) => {
+    console.log("응답:", res.data);
+  })
+  .catch((err) => {
+    if (err.response) {
+      console.error("상태:", err.response.status);
+      console.error("바디:", err.response.data);
+    } else {
+      console.error("에러:", err.message);
+    }
+  });
+*/
+
+axios
+  .get("http://127.0.0.1:3000/inquiry_answer", {
+    params: {
+      corpus: "약에 대한 복용법을 알려줘", // 엑셀 9번 질문 내용
+      //entpName: "한미약품", // 업체명
+      itemName: "타이레놀", // 제품명
+    },
+  })
+  .then((res) => {
+    console.log("응답:", res.data);
+  })
+  .catch((err) => {
+    if (err.response) {
+      console.error("상태:", err.response.status);
+      console.error("바디:", err.response.data);
+    } else {
+      console.error("에러:", err.message);
+    }
+  });
