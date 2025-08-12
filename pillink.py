f323a7122a9183bdbcedab0891cfff0c92fec619
Lee Suah <dltndk332@gmail.com>
2025-08-12 12:41:45 +0900

    port updated

--- pillink.py
+++ pillink.py
@@ -322,4 +322,5 @@ def ingredient_risk():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
